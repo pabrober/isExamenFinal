9f4ff90f013cd1098fccd7277c5064c6cb12cb82
Luma <luiswario@hotmail.es>
2020-06-11 23:59:30 -0600

    Inciso 6, servicio

--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
@@ -5,19 +5,30 @@
  */
 package gt.edu.umg.ingenieria.is.examen.examen.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import gt.edu.umg.ingenieria.is.examen.examen.service.AllService;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
  * @author pc
  */
+
 @RestController
 public class AllController {
-    
-    @PostMapping("/saludo")
-    public String Saludo(){
-       return "hola";               
+       
+    @Autowired
+    private AllService ser;
+    //Inciso 6 Hoja de excel Inicio
+      @GetMapping("/book/sheet")
+        public ArrayList<String> excel(@RequestParam Integer columns) {
+        return this.ser.excel(columns);
     }
+    //Inciso 6 Hoja de excel Fin
 }
+
 
--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
@@ -4,15 +4,31 @@
  * and open the template in the editor.
  */
 package gt.edu.umg.ingenieria.is.examen.examen.service;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author pc
  */
-public class AllService {
-    
-    
-    public static void main(String[] args) {		
-		System.out.println("Hola Mundo");
-	}
+@Service
+public class AllService  {
+   //Inciso 6 Hoja de excel Inicio
+   public String str(int i) {
+    return i < 0 ? "" : str((i / 26) - 1) + (char)(65 + i % 26);
+    //Inciso 6 Hoja de excel Fin
+}
+   //Inciso 6 Hoja de excel Inicio
+  public ArrayList<String> excel(Integer repeat) {
+   ArrayList<String> letras = new ArrayList<String>();
+   if(repeat >1 && repeat < 100000){
+   for (int i = 0; i < repeat; ++i) {
+       letras.add(this.str(i));
+   }
+  }
+      return letras;
+  }
+//Inciso 6 Hoja de excel Fin
 }
