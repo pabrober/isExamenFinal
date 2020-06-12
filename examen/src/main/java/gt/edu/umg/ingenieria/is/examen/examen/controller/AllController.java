7e94cdfe91d60f0713dd9f02df51eb52890073af
Josué Pivaral <Josu Pivaral@MJPA>
2020-06-11 23:43:11 -0600

    Creación de rama, creación de controllers  y services.

--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
@@ -1,23 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gt.edu.umg.ingenieria.is.examen.examen.controller;
-
-import org.springframework.web.bind.annotation.PostMapping;
+import gt.edu.umg.ingenieria.is.examen.examen.service.AllService;
+import java.util.ArrayList;
+import static java.util.Collections.reverse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author pc
- */
 @RestController
 public class AllController {
     
-    @PostMapping("/saludo")
-    public String Saludo(){
-       return "hola";               
+     @Autowired
+    public AllService allServices;
+    
+    @RequestMapping(value="selectSort/{elements}", method=RequestMethod.GET)
+    public ArrayList<Long> selectSort(@PathVariable ArrayList<Long> elements) throws Exception{
+    return this.allServices.selectSort(elements);
+    }
+    
+    @RequestMapping(value="bubbleSort/{elements}", method=RequestMethod.GET)
+    public ArrayList<Long> bubbleSort(@PathVariable ArrayList<Long> elements) throws Exception{
+    return this.allServices.bubbleSort(elements);
+    }
+    
+    @RequestMapping(value="insertionSort/{elements}", method=RequestMethod.GET)
+    public ArrayList<Long> insertionSort(@PathVariable ArrayList<Long> elements) throws Exception{
+    return this.allServices.insertionSort(elements);
+    }
+    
+    @RequestMapping(value="quickSort/{elements}", method=RequestMethod.GET)
+    public ArrayList<Long> quickSort(@PathVariable ArrayList<Long> elements) throws Exception{
+        ArrayList<Long> aux = this.allServices.quickSort(elements,0,elements.size()-1);
+        if(aux.size() > 1) {                   
+            Object value = aux.remove(0);
+            reverse(aux);
+            aux.add((Long) value);
+        }      
+      return aux;  
     }
 }
 
--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
@@ -1,14 +1,152 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gt.edu.umg.ingenieria.is.examen.examen.service;
+import java.util.ArrayList;
+import static java.util.Collections.reverse;
+import org.springframework.stereotype.Service;
 
-/**
- *
- * @author pc
- */
-public class AllService {
+@Service
+public class AllService extends Exception{
     
+//Exceptions
+    public AllService(){
+        super("Número de elementos fuera de rango");
+    }    
+    
+    
+//Algoritmo de ordenamiento por selección**************************************
+    public ArrayList<Long> selectSort(ArrayList<Long> elements) throws Exception, AllService{
+        ArrayList<Long> listaOrdenada = null;
+        if(elements.size()<2||elements.size()>1000){
+           throw new AllService();
+        }
+        int i,j,min;
+        long aux;
+        for (i=0;i<elements.size();i++){
+            min=i;
+            for(j=i+1;j<elements.size();j++){
+                if(elements.get(j)<elements.get(min)){
+                    min = j;
+                }
+            }
+            aux= elements.get(i);
+            elements.set(i, elements.get(min));
+            elements.set(min, aux);
+        }   
+        if(elements.size() > 1) {                   
+            Object value = elements.remove(0);
+            reverse(elements);
+            elements.add((Long) value);
+        }
+        listaOrdenada = elements;
+        return listaOrdenada; 
+    }
+    
+//Algoritmo de ordenamiento de burbuja**************************************
+    public ArrayList<Long> bubbleSort(ArrayList<Long> elements)throws Exception, AllService{
+        ArrayList<Long> listaOrdenada = null;
+        if(elements.size()<2||elements.size()>1000){
+           throw new AllService();
+        }
+        int i,j;
+        long aux;
+        for(i=0;i<elements.size()-1;i++){
+            for(j=0;j<elements.size()-1;j++){
+                if(elements.get(j)>elements.get(j+1)){
+                    aux = elements.get(j);
+                    elements.set(j, elements.get(j+1));
+                    elements.set(j+1, aux);
+                }
+            }
+        }
+        if(elements.size() > 1) {                   
+            Object value = elements.remove(0);
+            reverse(elements);
+            elements.add((Long) value);
+        }        
+        listaOrdenada = elements;
+        return listaOrdenada; 
+    }
+
+
+//Algoritmo de ordenamiento de burbuja**************************************
+    public ArrayList<Long> insertionSort(ArrayList<Long> elements)throws Exception, AllService{
+        ArrayList<Long> listaOrdenada = null;
+        if(elements.size()<2||elements.size()>1000){
+           throw new AllService();
+        }     
+        int i,j,pos;
+        long aux;
+        for(i=0;i<elements.size();i++){
+            pos = i;
+            aux = elements.get(i);
+            while((pos>0)&&(elements.get(pos-1)>aux)){
+                elements.set(pos, elements.get(pos-1));
+                pos--;
+            }
+            elements.set(pos, aux);
+        }
+        if(elements.size() > 1) {                   
+            Object value = elements.remove(0);
+            reverse(elements);
+            elements.add((Long) value);
+        }        
+        listaOrdenada = elements;
+        return listaOrdenada;
+    }
+    
+//Algoritmo de ordenamiento de quicksort**************************************   
+    public ArrayList<Long> quickSort(ArrayList<Long> elements,int primero, int ultimo)throws Exception, AllService{
+        ArrayList<Long> listaOrdenada = null;
+        if(elements.size()<2||elements.size()>1000){
+           throw new AllService();
+        }        
+        int i, j;
+        long aux,pivote;
+        i=primero;
+        j=ultimo;
+        pivote = elements.get((primero+ultimo)/2);
+        do{
+            while(elements.get(i)<pivote){
+                i++;
+            }
+            while (elements.get(j)>pivote){
+                j--;
+            }
+            if(i<=j){
+                aux = elements.get(i);
+                elements.set(i,elements.get(j));
+                elements.set(j,aux);
+                i++;
+                j--;
+            }
+        }while(i<=j);
+        if(primero<j){
+            quickSort(elements,primero,j);
+        }
+        if(i<ultimo){
+            quickSort(elements,i,ultimo);
+        }     
+        listaOrdenada = elements;
+        return listaOrdenada;
+    }
+ 
+    
+//Algoritmo de triangulo de pascal**************************************   
+    public ArrayList<String> pascalTriangle(int pisos)throws Exception, AllService{
+        ArrayList<String> aux = null;
+        ArrayList<String> lista = null;
+        for(int i=1; i<=pisos;i++){
+            ArrayList<String> pascal;
+            for(int k=pisos;k>i;k--){
+                lista.set(i,aux.get(i)+" ");
+            }
+            for(int j=0; j<i; j++){
+                if(j==0||j==(i-1)){
+                   // lista                   
+                }
+            }
+        }
+        
+        
+        return null;
+    }
 }
