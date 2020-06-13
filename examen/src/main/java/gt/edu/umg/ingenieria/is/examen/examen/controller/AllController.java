d5cf0efe449d1ce71eb8d802bff279d8c3d192b7
FernanoSolo <csolorznoh98@gmail.com>
2020-06-13 14:16:30 -0600

    Exception

--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/controller/AllController.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gt.edu.umg.ingenieria.is.examen.examen.controller;
-
-
-
-
 import gt.edu.umg.ingenieria.is.examen.examen.service.AllService.BinarySearchTree;
 import java.util.HashMap;
 import java.util.List;
@@ -19,25 +10,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- *
- * @author pc
- */
 @RestController
 public class AllController {
 
     @Autowired
     private BinarySearchTree tree;
 
-    //@GetMapping("tree/binary")
-    //public void  addValues(@RequestParam(name = "data") int elements[]) {
-    //    this.tree.main(elements);
-    //}
-    
     @GetMapping("tree/binary")
-    public HashMap<String, List<Integer>> addValues(@RequestParam(name = "data") int elements[]) {    
-    return this.tree.main(elements);
-        //return this.ser.proporcionPobla(generation);
+    public HashMap<String, List<Integer>> addValues(@RequestParam(name = "data") int elements[])throws Exception {    
+    return this.tree.createTree(elements);
+     
     }
 
 }
--- examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
+++ examen/src/main/java/gt/edu/umg/ingenieria/is/examen/examen/service/AllService.java
@@ -1,10 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package gt.edu.umg.ingenieria.is.examen.examen.service;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import static java.util.Collections.list;
@@ -14,19 +8,21 @@ import java.util.Map;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-/**
- *
- * @author pc
- */
+
 @Transactional
 @Service
-public class AllService {
+public class AllService extends Exception {
 
+    
+  public AllService(){
+        super("Número de elementos fuera de rango");
+    }    
+    
     @Transactional
     @Service
     public class BinarySearchTree {
 
-        /* Class containing left and right child of current node and key value*/
+        
         class Node {
 
             int key;
@@ -38,31 +34,31 @@ public class AllService {
             }
         }
 
-        // Root 
+         
         Node root;
         List<Integer> x = new ArrayList<Integer>();
         List<Integer> z = new ArrayList<Integer>();
         List<Integer> y = new ArrayList<Integer>();
-        // Constructor 
+         
         BinarySearchTree() {
             root = null;
         }
 
-        // This method mainly calls insertRec() 
+         
         void insert(int key) {
             root = insertRec(root, key);
         }
 
-        /* A recursive function to insert a new key in BT */
+        
         Node insertRec(Node root, int key) {
 
-            /* If the tree is empty, return a new node */
+           
             if (root == null) {
                 root = new Node(key);
                 return root;
             }
 
-            /* Otherwise, recur down the tree */
+           
             if (key < root.key) {
                 root.left = insertRec(root.left, key);
             } else if (key > root.key) {
@@ -73,21 +69,21 @@ public class AllService {
             return root;
         }
 
-        // This method mainly calls InorderRec() 
+         
         List<Integer> inorder() {
              return inorderRec(root);
         }
 
-        // A utility function to do inorder traversal of BT 
+         
         List<Integer> inorderRec(Node root) {
             
             
             if (root != null) {
                 inorderRec(root.left);
                 x.add(root.key);    
-                //System.out.println(root.key);
+                
                 inorderRec(root.right);
-                //return map;
+                
             }
            return x;
         }
@@ -100,10 +96,9 @@ public class AllService {
             
             if (root != null) {
                 z.add(root.key);
-                //System.out.print(" \n" + root.key);
                 preorderRec(root.left);
                 preorderRec(root.right);
-                //return map;
+   
             }
             return z;
         }
@@ -118,49 +113,34 @@ public class AllService {
                 postorderRec(root.left);
                 postorderRec(root.right);
                 y.add(root.key);
-                //System.out.print(" \n" + root.key);
-                
-            //return map;
+   
             }
             return y;
         }
 
-        // Driver Program to test above functions 
-        public  HashMap<String, List<Integer>> main(int elements[]) {
+
+        public  HashMap<String, List<Integer>> createTree(int elements[]) throws Exception, AllService {
+           if(elements.length<2||elements.length >1000){
+           throw new AllService();
+        }
+            
             BinarySearchTree tree = new BinarySearchTree();
             
             
-            HashMap<String, List<Integer>> subjects = new HashMap<>();
+            HashMap<String, List<Integer>> search = new HashMap<>();
             
-            
-            
-            //Map<String, List<Integer>> map = new HashMap<String, List<Integer>>();
-            
-            //map.put("Inorder", new ArrayList<Integer>(tree.inorder()));
-           //map.put("mango", new ArrayList<Integer>((tree.inorder())));
-           
-            //subjects = new HashMap<String, Integer[]>();
-           // subjects.put("calculus", tree.inorder());
-
-            /* Let us create following Tree 
-              50 
-           /     \ 
-          30      70 
-         /  \    /  \ 
-       20   40  60   80 */
             
             for (int i = 0; i < elements.length; i++) {
                 tree.insert(elements[i]);
 
-                // print inorder traversal of the BST
             }
             
-            subjects.put("Pre Order", tree.preorder());
-            subjects.put("In Order", tree.inorder());
-            subjects.put("Post Order", tree.postorder());
+            search.put("Pre Order", tree.preorder());
+            search.put("In Order", tree.inorder());
+            search.put("Post Order", tree.postorder());
             
             
-            return subjects;
+            return search;
 
         }
     }
